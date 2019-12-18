0cf99de92e780a95312066b449d4c1e2e566ec89
Ben Hernandez <benjammin234@gmail.com>
2019-12-18 10:44:03 -0800

    as usual, something is wrong

--- script.js
+++ script.js
@@ -6,23 +6,24 @@ $(function() {
 });
 
 function watchForm() {
-    $('form').submit(event => {
+    $('#dog-num').submit(event => {
         event.preventDefault();
-        getDogImages();
+        let userNumInput = $('#dog-num').val();
+        getDogImages(userNumInput);
     });
 }
 
 // n should go where the 3 is in the endpoint url
 function getDogImages(n) {
-        fetch('https://dog.ceo/api/breeds/image/random/3')
+        fetch(`https://dog.ceo/api/breeds/image/random/${n}`)
         .then(response => response.json())
-        .then(responseJson => displayResults(responseJson))
+        .then(responseJson => console.log(responseJson))
         .catch(error => alert("Apologies, one of the puppies had an accident and we're cleaning it up. Please try again in a few moments."));
     
 }
 
-function displayResults(responseJson) {
-    console.log(responseJson.message);
-    $('.results-img').replaceWith(`<img src="${responseJson.message}" class="results-img">`);
-    $('.results-img').removeClass('hidden');
-}
+// function displayResults(responseJson) {
+//     console.log(responseJson.message);
+//     $('.results-img').replaceWith(`<img src="${responseJson.message}" class="results-img">`);
+//     $('.results-img').removeClass('hidden');
+// }
