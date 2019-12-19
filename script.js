8955d0115ccd3bf3e3e8a563bdcc3bf7ded1d027
Ben Hernandez <benjammin234@gmail.com>
2019-12-18 17:13:25 -0800

    corrected an id mistake

--- script.js
+++ script.js
@@ -8,14 +8,16 @@ $(function() {
 function watchForm() {
     $('#dog-num').submit(event => {
         event.preventDefault();
-        let userNumInput = $('#dog-num').val();
+        let userNumInput = $('#numDogs').val();
         getDogImages(userNumInput);
     });
 }
 
 // n should go where the 3 is in the endpoint url
 function getDogImages(n) {
-        fetch(`https://dog.ceo/api/breeds/image/random/${n}`)
+        fetch(`https://dog.ceo/api/breeds/image/random/${n}`, {
+            credentials: "include"
+        })
         .then(response => response.json())
         .then(responseJson => console.log(responseJson))
         .catch(error => alert("Apologies, one of the puppies had an accident and we're cleaning it up. Please try again in a few moments."));
