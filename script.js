dbdfcca3a0f51bf5deee9723bf3c13afa4b6004e
Ben Hernandez <benjammin234@gmail.com>
2019-12-19 19:58:19 -0800

    fully functioning!

--- script.js
+++ script.js
@@ -9,21 +9,23 @@ function watchForm() {
     $('#dog-num').submit(event => {
         event.preventDefault();
         let userNumInput = $('#numDogs').val();
+        $('.results').empty();
         getDogImages(userNumInput);
     });
 }
 
-// n should go where the 3 is in the endpoint url
 function getDogImages(n) {
         fetch(`https://dog.ceo/api/breeds/image/random/${n}`)
         .then(response => response.json())
-        .then(responseJson => console.log(responseJson))
+        .then(responseJson => displayResults(responseJson))
         .catch(error => alert("Apologies, one of the puppies had an accident and we're cleaning it up. Please try again in a few moments."));
     
 }
 
-// function displayResults(responseJson) {
-//     console.log(responseJson.message);
-//     $('.results-img').replaceWith(`<img src="${responseJson.message}" class="results-img">`);
-//     $('.results-img').removeClass('hidden');
-// }
+function displayResults(responseJson) {
+    console.log(responseJson.message);
+    responseJson.message.forEach(element => {
+        $('.results').append(`<img src="${element}" class="results">`);
+    })
+    $('.results').removeClass('hidden');
+}
